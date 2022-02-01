830c72363711fc9dfe296d3afa0ccb0f6825a85e
Faturrahman Ma'ruf <pedro@MacBook-Air-M1.local>
2022-02-01 21:59:35 +0700

    fix public subnet

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_vpc" "dev" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.dev
+  vpc_id = aws_vpc.dev.id
 
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "ap-southeast-1a"
